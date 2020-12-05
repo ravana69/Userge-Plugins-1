06b83f5d9297d194089516bdc73f01736916211d
Ravana <47528708+ravana69@users.noreply.github.com>
2020-12-05 11:24:37 +0530

    Add files via upload

---
+++ plugins/detail.py
@@ -0,0 +1,12 @@
+import asyncio
+import random
+from userge import Message, userge
+@userge.on_cmd(
+    "detail",
+    about={
+        "header": "detect details of user",
+        "usage": "{tr}reply to detect details",
+    },
+)
+async def detail_hi(message: Message):
+  await message.edit(f"{(await userge.get_users(message.reply_to_message.from_user.id)).first_name} has: \n**1. IQ :** {random.choice(range(0,1000))}% \n**2. Dick Size :** {random.choice(range(0,100))} Centimetres \n**3. Lie Meter reading :** {random.choice(range(0,100))}% \n**4. Gey Count :** {random.choice(range(0,100))}%")
