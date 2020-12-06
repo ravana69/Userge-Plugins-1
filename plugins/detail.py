305cfc0ede7a0ea6f3a4baba40c93fdf0f3e4c94
Ravana <47528708+ravana69@users.noreply.github.com>
2020-12-06 17:51:17 +0530

    indian size

--- plugins/detail.py
+++ plugins/detail.py
@@ -9,4 +9,4 @@ from userge import Message, userge
     },
 )
 async def detail_hi(message: Message):
-  await message.edit(f"{(await userge.get_users(message.reply_to_message.from_user.id)).first_name} has: \n**1. IQ :** {random.choice(range(0,1000))}% \n**2. Dick Size :** {random.choice(range(0,100))} Centimetres \n**3. Lie Meter reading :** {random.choice(range(0,100))}% \n**4. Gey Count :** {random.choice(range(0,100))}%")
+  await message.edit(f"{(await userge.get_users(message.reply_to_message.from_user.id)).first_name} has: \n**1. IQ :** {random.choice(range(0,1000))}% \n**2. Dick Size :** {random.choice(range(0,10))} Centimetres \n**3. Lie Meter reading :** {random.choice(range(0,100))}% \n**4. Gey Count :** {random.choice(range(0,100))}%")
