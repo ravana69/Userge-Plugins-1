3e6e88ab50aaf6c9ca2b89bcb10beb4dd3f673e1
Ravana <47528708+ravana69@users.noreply.github.com>
2021-01-18 17:18:12 +0530

    Update fbun.py

--- plugins/fbun.py
+++ plugins/fbun.py
@@ -9,4 +9,4 @@ from userge import Message, userge
     },
 )
 async def detail_hi(message: Message):
-  await message.edit(f"{(await userge.get_users(message.reply_to_message.from_user.id)).first_name} has been banned from: \n**1. Direct Message** :**Yes**\n**2. Public Group :** {random.choice(range(0,99))} Groups\n**3. Public Channel** : {random.choice(range(0,99))} Channels\n**4. Private Group :** {random.choice(range(0,99))} Groups\n**5. Private Channel :** {random.choice(range(0,99))} Channels")
+  await message.edit(f"{(await userge.get_users(message.reply_to_message.from_user.id)).first_name} has been fbunned from: \n**1. Direct Message** : **Yes**\n**2. Public Group :** {random.choice(range(0,99))} Groups\n**3. Public Channel** : {random.choice(range(0,99))} Channels\n**4. Private Group :** {random.choice(range(0,99))} Groups\n**5. Private Channel :** {random.choice(range(0,99))} Channels")
