f0f53c862f26f7b24ca589d52602e579926a9c09
Ravana <47528708+ravana69@users.noreply.github.com>
2021-01-29 15:30:14 +0530

    Update detail.py
    Afrikans stay out from this plugin

--- plugins/detail.py
+++ plugins/detail.py
@@ -4,9 +4,9 @@ from userge import Message, userge
 @userge.on_cmd(
     "detail",
     about={
-        "header": "detect details of user",
-        "usage": "{tr}reply to detect details",
+        "header": "detect details of user. Ignore measurement if you are Afrikan",
+        "usage": "{tr}reply to detect details. Dont reply any Afrikans",
     },
 )
 async def detail_hi(message: Message):
-  await message.edit(f"{(await userge.get_users(message.reply_to_message.from_user.id)).first_name} has: \n**1. IQ :** {random.choice(range(0,1000))}% \n**2. Dik Size :** {random.choice(range(0,10))} Centimetres \n**3. Lie Meter reading :** {random.choice(range(0,100))}% \n**4. Gey Count :** {random.choice(range(0,100))}%")
+  await message.edit(f"{(await userge.get_users(message.reply_to_message.from_user.id)).first_name} has: \n**1. IQ :** {random.choice(range(0,1000))}% \n**2. Dik Size :** {random.choice(range(0,10))} centimeter\n**3. Lie Meter Reading :** {random.choice(range(0,100))}% \n**4. Gey Count :** {random.choice(range(0,100))}%")
